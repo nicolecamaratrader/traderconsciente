14855a34ce32bb26a2386ec74e1b07bd02c267f7
Nicole Câmara Trader <n98camara@gmail.com>
2024-07-22 16:06:12 -0400

    Função direcionar ao formulário em cada botão aplicada

--- index.html/index.js
+++ index.html/index.js
@@ -6,3 +6,18 @@ function openebook () {
 function closeebook () {
     document.getElementById("coveronphone").src="./imagens.png/coveronphone.png";
 }
+
+function scrollToForm () {
+    document.getElementById('primeiropasso').scrollIntoView ({bahevior:'smooth'});
+    document.getElementById('nome').focus();
+}
+
+function scrollToForm () {
+    document.getElementById('sabermais').scrollIntoView ({bahevior:'smooth'});
+    document.getElementById('nome').focus();
+}
+
+function scrollToForm () {
+    document.getElementById('adquirir').scrollIntoView ({bahevior:'smooth'});
+    document.getElementById('nome').focus();
+}
